a5c715fbfe3c6a91b186747c7d19d722b28fd49c
Francesc Alted <faltet@pytables.org>
2004-01-16 17:05:44 +0000

    Added a compatibilibty hack for long long types in both python 2.2 and python 2.3
    git-svn-id: http://www.pytables.org/svn/pytables/trunk@236 1b98710c-d8ec-0310-ae81-f5f2bcd8cb94

--- src/type-longlong.h
+++ src/type-longlong.h
@@ -7,3 +7,10 @@
 #   define MY_MSC 0
 #endif /*_MSC_VER*/
 
+
+/* It seems python 2.2 defines LONG_LONG and python 2.3 defines
+   PY_LONG_LONG.  There's a well published and easy check for this:
+ */
+#if !defined(PY_LONG_LONG) && defined(LONG_LONG) 
+#define PY_LONG_LONG LONG_LONG
+#endif
