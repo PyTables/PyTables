58c1031f1a99257d369b59a63ec7a05dd9a453b1
Francesc Alted <faltet@pytables.org>
2004-12-17 16:38:24 +0000

    Added missing exceptions.py
    git-svn-id: http://www.pytables.org/svn/pytables/trunk@426 1b98710c-d8ec-0310-ae81-f5f2bcd8cb94

---
+++ tables/exceptions.py
@@ -0,0 +1,16 @@
+########################################################################
+#
+#       License: BSD
+#       Created: December 17, 2004
+#       Author:  Francesc Altet - faltet@carabos.com
+#
+#       $Source: /home/ivan/_/programari/pytables/svn/cvs/pytables/pytables/tables/exceptions.py,v $
+#       $Id: exceptions.py,v 1.1 2004/12/17 16:38:24 falted Exp $
+#
+########################################################################
+
+"""Declare exceptions and warnings that are specific to PyTables"""
+
+class NaturalNameWarning(Warning):
+    "To warn the user about non-valid names in natural naming schema"
+    pass
