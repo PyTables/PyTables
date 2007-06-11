5be98437cafa1db4d08b0117a8eb7c006345a2d5
Ivan Vilata i Balaguer <ivilata@carabos.com>
2007-06-11 11:36:47 +0000

    Added background image, welcome message and merged license file to Mac installer.
    git-svn-id: http://www.pytables.org/svn/pytables/branches/professional@3014 1b98710c-d8ec-0310-ae81-f5f2bcd8cb94

--- macosx/make.sh
+++ macosx/make.sh
@@ -1,33 +1,47 @@
 #!/bin/sh
 set -e
 
-VER=$(cat ../VERSION)
 PYVERS="2.4 2.5"
 PMPROJ_TMPL="pytables-@VER@-py@PYVER@.pmproj"
+WELCOME_TMPL="welcome-@VER@-py@PYVER@.rtf"
+BACKGROUND="background.tif"
+SUBPKGS="hdf5-1.6.5.pkg numpy-1.0.3"
 
-SUBPKGS="hdf5-1.6.5.pkg numpy-1.0.3 SELF"
+VER=$(cat ../VERSION)
+VERNP=${VER%pro}
+WELCOME_EXT=$(echo "$WELCOME_TMPL" | sed -ne 's/.*\.\(.*\)/\1/p')
+SUBPKGS="SELF $SUBPKGS"
 
 packagemaker=/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker
 
 for PYVER in $PYVERS; do
-	PMPROJ=$(echo "$PMPROJ_TMPL" | sed -e "s/@VER@/$VER/" -e "s/@PYVER@/$PYVER/")
-	MPKG="PyTables Pro $VER for Python $PYVER.mpkg"
+	PMPROJ=$(echo "$PMPROJ_TMPL" | sed -e "s/@VER@/$VER/" -e "s/@VERNP@/$VERNP/" -e "s/@PYVER@/$PYVER/")
+	WELCOME=$(echo "$WELCOME_TMPL" | sed -e "s/@VER@/$VER/" -e "s/@VERNP@/$VERNP/" -e "s/@PYVER@/$PYVER/")
+	MPKG="PyTables Pro $VERNP for Python $PYVER.mpkg"
 
 	if [ "$1" = "clean" ]; then
-		rm -rf "$PMPROJ" "$MPKG" *.bak
+		rm -rf "$WELCOME" "$PMPROJ" "$MPKG" *.bak
 		continue
 	fi
 
+	echo "Creating $WELCOME..."
+	sed -e "s/@VER@/$VER/g" -e "s/@VERNP@/$VERNP/g" -e "s/@PYVER@/$PYVER/g" < "$WELCOME_TMPL" > "$WELCOME"
+
 	echo "Creating $PMPROJ..."
 	plutil -convert xml1 -o "$PMPROJ" "$PMPROJ_TMPL"
-
-	sed -i .bak -e "s/@VER@/$VER/g" -e "s/@PYVER@/$PYVER/g" "$PMPROJ"
+	sed -i .bak -e "s/@VER@/$VER/g" -e "s/@VERNP@/$VERNP/g" -e "s/@PYVER@/$PYVER/g" "$PMPROJ"
 
 	echo "Building $MPKG..."
 	# Avoiding the verbose flag makes building fail! ;(
 	$packagemaker -build -proj "$PMPROJ" -p "$MPKG" -v
 
+	echo "Fixing $MPKG..."
+	cp "$WELCOME" "$MPKG/Contents/Resources/Welcome.$WELCOME_EXT"
+	cp "$BACKGROUND" "$MPKG/Contents/Resources"
+
 	echo -n "Adding subpackages..."
+	LICENSE="$MPKG/Contents/Resources/License.txt"
+	true > "$LICENSE"
 	for SUBPKG in $SUBPKGS; do
 		echo -n " $SUBPKG"
 		if [ "$SUBPKG" = "SELF" ]; then
@@ -38,6 +52,14 @@ for PYVER in $PYVERS; do
 			SUBPKG="$(echo ../../$SUBPKG/dist/$SUBPKG-py$PYVER*pkg)"
 		fi
 		cp -R "$SUBPKG" "$MPKG/Contents/Packages"
+
+		PKGRES="$SUBPKG/Contents/Resources"
+		if test -f "$PKGRES/License.txt"; then
+			cat $_ >> "$LICENSE"
+		else
+			cat "$PKGRES/English.lproj/License.txt" >> "$LICENSE"
+		fi
+		echo -e "\n--------------------------------\n" >> "$LICENSE"
 	done
 	echo
 done
