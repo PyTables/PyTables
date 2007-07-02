ed89fa24af8a2cb00902896f594a38e7295b59ce
Ivan Vilata i Balaguer <ivilata@carabos.com>
2007-07-02 17:09:14 +0000

    Now, really build packages for all licenses under Mac OS X.
    The seemingly large amount of changes is only a result of reindenting a large
    block of code into yet another for loop.
    
    
    git-svn-id: http://www.pytables.org/svn/pytables/branches/professional@3085 1b98710c-d8ec-0310-ae81-f5f2bcd8cb94

--- macosx/make.sh
+++ macosx/make.sh
@@ -2,6 +2,7 @@
 set -e
 
 PYVERS="2.4 2.5"
+LICENSES="personal site development"
 PMPROJ_TMPL="pytables-@VER@-@LIC@-py@PYVER@.pmproj"
 WELCOME_TMPL="welcome-@VER@-@LIC@-py@PYVER@.rtf"
 BACKGROUND="background.tif"
@@ -18,74 +19,73 @@ if [ "$1" = "clean" ]; then
 	cleaning=true
 fi
 
-##XXX
-LIC=personal
-##XXX
-for PYVER in $PYVERS; do
-	PMPROJ=$(echo "$PMPROJ_TMPL" | sed -e "s/@VER@/$VER/" -e "s/@VERNP@/$VERNP/" -e "s/@LIC@/$LIC/" -e "s/@PYVER@/$PYVER/")
-	WELCOME=$(echo "$WELCOME_TMPL" | sed -e "s/@VER@/$VER/" -e "s/@VERNP@/$VERNP/" -e "s/@LIC@/$LIC/" -e "s/@PYVER@/$PYVER/")
-	MPKG="PyTables Pro $VERNP ($LIC) for Python $PYVER.mpkg"
-	LICENSE="$MPKG/Contents/Resources/License.txt"
-	DMGDIR="PyTables Pro $VERNP $LIC (py$PYVER)"
-	DMG="PyTablesPro-${VERNP}-${LIC}.macosx-py${PYVER}.dmg"
-
-	if [ $cleaning ]; then
-		rm -rf "$WELCOME" "$PMPROJ" "$MPKG" "$DMGDIR" "$DMG" *.bak
-		continue
-	fi
-
-	echo "Creating $WELCOME..."
-	sed -e "s/@VER@/$VER/g" -e "s/@VERNP@/$VERNP/g" -e "s/@LIC@/$LIC/g" -e "s/@PYVER@/$PYVER/g" < "$WELCOME_TMPL" > "$WELCOME"
-
-	echo "Creating $PMPROJ..."
-	plutil -convert xml1 -o "$PMPROJ" "$PMPROJ_TMPL"
-	sed -i .bak -e "s/@VER@/$VER/g" -e "s/@VERNP@/$VERNP/g" -e "s/@LIC@/$LIC/g" -e "s/@PYVER@/$PYVER/g" "$PMPROJ"
-
-	echo "Building $MPKG..."
-	# Avoiding the verbose flag makes building fail! ;(
-	$packagemaker -build -proj "$PMPROJ" -p "$MPKG" -v
-
-	echo "Fixing $MPKG..."
-	cp "$WELCOME" "$MPKG/Contents/Resources/Welcome.$WELCOME_EXT"
-	cp "$BACKGROUND" "$MPKG/Contents/Resources"
-
-	echo -n "Adding subpackages..."
-	true > "$LICENSE"
-	for SUBPKG in $SUBPKGS; do
-		echo -n " $SUBPKG"
-		if [ "$SUBPKG" = "SELF" ]; then
-			PKGSRC="$(echo ../dist/tables-$VER-py$PYVER*pkg)"
-		elif [ $(expr "$SUBPKG" : ".*\.pkg") != 0 ]; then
-			PKGSRC="../../$SUBPKG"
-		else
-			PKGSRC="$(echo ../../$SUBPKG/dist/$SUBPKG-py$PYVER*pkg)"
+for LIC in $LICENSES; do
+	for PYVER in $PYVERS; do
+		PMPROJ=$(echo "$PMPROJ_TMPL" | sed -e "s/@VER@/$VER/" -e "s/@VERNP@/$VERNP/" -e "s/@LIC@/$LIC/" -e "s/@PYVER@/$PYVER/")
+		WELCOME=$(echo "$WELCOME_TMPL" | sed -e "s/@VER@/$VER/" -e "s/@VERNP@/$VERNP/" -e "s/@LIC@/$LIC/" -e "s/@PYVER@/$PYVER/")
+		MPKG="PyTables Pro $VERNP ($LIC) for Python $PYVER.mpkg"
+		LICENSE="$MPKG/Contents/Resources/License.txt"
+		DMGDIR="PyTables Pro $VERNP $LIC (py$PYVER)"
+		DMG="PyTablesPro-${VERNP}-${LIC}.macosx-py${PYVER}.dmg"
+	
+		if [ $cleaning ]; then
+			rm -rf "$WELCOME" "$PMPROJ" "$MPKG" "$DMGDIR" "$DMG" *.bak
+			continue
 		fi
-		cp -R "$PKGSRC" "$MPKG/Contents/Packages"
-
-		PKGRES="$PKGSRC/Contents/Resources"
-		if [ "$SUBPKG" = "SELF" ]; then
-			# Place the proper license in all PyTables packages.
-			PKGDST="$MPKG/Contents/Packages/$(basename $PKGSRC)"
-			find "$PKGDST" -path "*/Contents/Resources" -type d \
-				-exec cp "../LICENSE-$LIC.txt" '{}/License.txt' ';'
-			cat "../LICENSE-$LIC.txt" >> "$LICENSE"
-		elif test -f "$PKGRES/License.txt"; then
-			cat $_ >> "$LICENSE"
-		else
-			cat "$PKGRES/English.lproj/License.txt" >> "$LICENSE"
-		fi
-		echo -e "\n--------------------------------\n" >> "$LICENSE"
+	
+		echo "Creating $WELCOME..."
+		sed -e "s/@VER@/$VER/g" -e "s/@VERNP@/$VERNP/g" -e "s/@LIC@/$LIC/g" -e "s/@PYVER@/$PYVER/g" < "$WELCOME_TMPL" > "$WELCOME"
+	
+		echo "Creating $PMPROJ..."
+		plutil -convert xml1 -o "$PMPROJ" "$PMPROJ_TMPL"
+		sed -i .bak -e "s/@VER@/$VER/g" -e "s/@VERNP@/$VERNP/g" -e "s/@LIC@/$LIC/g" -e "s/@PYVER@/$PYVER/g" "$PMPROJ"
+	
+		echo "Building $MPKG..."
+		# Avoiding the verbose flag makes building fail! ;(
+		$packagemaker -build -proj "$PMPROJ" -p "$MPKG" -v
+	
+		echo "Fixing $MPKG..."
+		cp "$WELCOME" "$MPKG/Contents/Resources/Welcome.$WELCOME_EXT"
+		cp "$BACKGROUND" "$MPKG/Contents/Resources"
+	
+		echo -n "Adding subpackages..."
+		true > "$LICENSE"
+		for SUBPKG in $SUBPKGS; do
+			echo -n " $SUBPKG"
+			if [ "$SUBPKG" = "SELF" ]; then
+				PKGSRC="$(echo ../dist/tables-$VER-py$PYVER*pkg)"
+			elif [ $(expr "$SUBPKG" : ".*\.pkg") != 0 ]; then
+				PKGSRC="../../$SUBPKG"
+			else
+				PKGSRC="$(echo ../../$SUBPKG/dist/$SUBPKG-py$PYVER*pkg)"
+			fi
+			cp -R "$PKGSRC" "$MPKG/Contents/Packages"
+	
+			PKGRES="$PKGSRC/Contents/Resources"
+			if [ "$SUBPKG" = "SELF" ]; then
+				# Place the proper license in all PyTables packages.
+				PKGDST="$MPKG/Contents/Packages/$(basename $PKGSRC)"
+				find "$PKGDST" -path "*/Contents/Resources" -type d \
+					-exec cp "../LICENSE-$LIC.txt" '{}/License.txt' ';'
+				cat "../LICENSE-$LIC.txt" >> "$LICENSE"
+			elif test -f "$PKGRES/License.txt"; then
+				cat $_ >> "$LICENSE"
+			else
+				cat "$PKGRES/English.lproj/License.txt" >> "$LICENSE"
+			fi
+			echo -e "\n--------------------------------\n" >> "$LICENSE"
+		done
+		echo
+	
+		echo "Building $DMG..."
+		mkdir -p "$DMGDIR"
+		mv "$MPKG" "$DMGDIR"
+		cp ../README.txt "$DMGDIR/ReadMe.txt"
+		mkdir -p "$DMGDIR/Examples"
+		cp -R ../examples/* "$DMGDIR/Examples/"
+		cp ../doc/usersguide.pdf "$DMGDIR/User's Guide.pdf"
+		cp -R ../doc/html "$DMGDIR/User's Guide (HTML)"
+		hdiutil create -srcfolder "$DMGDIR" -anyowners -format UDZO -imagekey zlib-level=9 "$DMG"
 	done
-	echo
-
-	echo "Building $DMG..."
-	mkdir -p "$DMGDIR"
-	mv "$MPKG" "$DMGDIR"
-	cp ../README.txt "$DMGDIR/ReadMe.txt"
-	mkdir -p "$DMGDIR/Examples"
-	cp -R ../examples/* "$DMGDIR/Examples/"
-	cp ../doc/usersguide.pdf "$DMGDIR/User's Guide.pdf"
-	cp -R ../doc/html "$DMGDIR/User's Guide (HTML)"
-	hdiutil create -srcfolder "$DMGDIR" -anyowners -format UDZO -imagekey zlib-level=9 "$DMG"
 done
 echo "Done"
