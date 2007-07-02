562994ed174c05e3c5f0c15410cdd80ec7088ddc
Ivan Vilata i Balaguer <ivilata@carabos.com>
2007-07-02 16:55:25 +0000

    Take the license kind into account when generating the Mac OS X packages.
    Right now only `personal` is used.  The others in the next commit.
    
    
    git-svn-id: http://www.pytables.org/svn/pytables/branches/professional@3080 1b98710c-d8ec-0310-ae81-f5f2bcd8cb94

--- macosx/make.sh
+++ macosx/make.sh
@@ -2,8 +2,8 @@
 set -e
 
 PYVERS="2.4 2.5"
-PMPROJ_TMPL="pytables-@VER@-py@PYVER@.pmproj"
-WELCOME_TMPL="welcome-@VER@-py@PYVER@.rtf"
+PMPROJ_TMPL="pytables-@VER@-@LIC@-py@PYVER@.pmproj"
+WELCOME_TMPL="welcome-@VER@-@LIC@-py@PYVER@.rtf"
 BACKGROUND="background.tif"
 SUBPKGS="hdf5-1.6.5.pkg numpy-1.0.3"
 
@@ -18,13 +18,16 @@ if [ "$1" = "clean" ]; then
 	cleaning=true
 fi
 
+##XXX
+LIC=personal
+##XXX
 for PYVER in $PYVERS; do
-	PMPROJ=$(echo "$PMPROJ_TMPL" | sed -e "s/@VER@/$VER/" -e "s/@VERNP@/$VERNP/" -e "s/@PYVER@/$PYVER/")
-	WELCOME=$(echo "$WELCOME_TMPL" | sed -e "s/@VER@/$VER/" -e "s/@VERNP@/$VERNP/" -e "s/@PYVER@/$PYVER/")
-	MPKG="PyTables Pro $VERNP for Python $PYVER.mpkg"
+	PMPROJ=$(echo "$PMPROJ_TMPL" | sed -e "s/@VER@/$VER/" -e "s/@VERNP@/$VERNP/" -e "s/@LIC@/$LIC/" -e "s/@PYVER@/$PYVER/")
+	WELCOME=$(echo "$WELCOME_TMPL" | sed -e "s/@VER@/$VER/" -e "s/@VERNP@/$VERNP/" -e "s/@LIC@/$LIC/" -e "s/@PYVER@/$PYVER/")
+	MPKG="PyTables Pro $VERNP ($LIC) for Python $PYVER.mpkg"
 	LICENSE="$MPKG/Contents/Resources/License.txt"
-	DMGDIR="PyTables Pro $VERNP (py$PYVER)"
-	DMG="PyTables-Pro-${VERNP}_py${PYVER}.dmg"
+	DMGDIR="PyTables Pro $VERNP $LIC (py$PYVER)"
+	DMG="PyTablesPro-${VERNP}-${LIC}.macosx-py${PYVER}.dmg"
 
 	if [ $cleaning ]; then
 		rm -rf "$WELCOME" "$PMPROJ" "$MPKG" "$DMGDIR" "$DMG" *.bak
@@ -32,11 +35,11 @@ for PYVER in $PYVERS; do
 	fi
 
 	echo "Creating $WELCOME..."
-	sed -e "s/@VER@/$VER/g" -e "s/@VERNP@/$VERNP/g" -e "s/@PYVER@/$PYVER/g" < "$WELCOME_TMPL" > "$WELCOME"
+	sed -e "s/@VER@/$VER/g" -e "s/@VERNP@/$VERNP/g" -e "s/@LIC@/$LIC/g" -e "s/@PYVER@/$PYVER/g" < "$WELCOME_TMPL" > "$WELCOME"
 
 	echo "Creating $PMPROJ..."
 	plutil -convert xml1 -o "$PMPROJ" "$PMPROJ_TMPL"
-	sed -i .bak -e "s/@VER@/$VER/g" -e "s/@VERNP@/$VERNP/g" -e "s/@PYVER@/$PYVER/g" "$PMPROJ"
+	sed -i .bak -e "s/@VER@/$VER/g" -e "s/@VERNP@/$VERNP/g" -e "s/@LIC@/$LIC/g" -e "s/@PYVER@/$PYVER/g" "$PMPROJ"
 
 	echo "Building $MPKG..."
 	# Avoiding the verbose flag makes building fail! ;(
@@ -51,16 +54,22 @@ for PYVER in $PYVERS; do
 	for SUBPKG in $SUBPKGS; do
 		echo -n " $SUBPKG"
 		if [ "$SUBPKG" = "SELF" ]; then
-			SUBPKG="$(echo ../dist/tables-$VER-py$PYVER*pkg)"
+			PKGSRC="$(echo ../dist/tables-$VER-py$PYVER*pkg)"
 		elif [ $(expr "$SUBPKG" : ".*\.pkg") != 0 ]; then
-			SUBPKG="../../$SUBPKG"
+			PKGSRC="../../$SUBPKG"
 		else
-			SUBPKG="$(echo ../../$SUBPKG/dist/$SUBPKG-py$PYVER*pkg)"
+			PKGSRC="$(echo ../../$SUBPKG/dist/$SUBPKG-py$PYVER*pkg)"
 		fi
-		cp -R "$SUBPKG" "$MPKG/Contents/Packages"
+		cp -R "$PKGSRC" "$MPKG/Contents/Packages"
 
-		PKGRES="$SUBPKG/Contents/Resources"
-		if test -f "$PKGRES/License.txt"; then
+		PKGRES="$PKGSRC/Contents/Resources"
+		if [ "$SUBPKG" = "SELF" ]; then
+			# Place the proper license in all PyTables packages.
+			PKGDST="$MPKG/Contents/Packages/$(basename $PKGSRC)"
+			find "$PKGDST" -path "*/Contents/Resources" -type d \
+				-exec cp "../LICENSE-$LIC.txt" '{}/License.txt' ';'
+			cat "../LICENSE-$LIC.txt" >> "$LICENSE"
+		elif test -f "$PKGRES/License.txt"; then
 			cat $_ >> "$LICENSE"
 		else
 			cat "$PKGRES/English.lproj/License.txt" >> "$LICENSE"
