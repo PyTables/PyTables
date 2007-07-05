f907645e10a864bcd549d707112fb5e4a936805e
Francesc Alted <faltet@pytables.org>
2007-07-05 18:12:49 +0000

    Updated the resources that will take the heavy suite in the Pro version (i.e. a *lot more* than with standard).
    git-svn-id: http://www.pytables.org/svn/pytables/branches/professional@3106 1b98710c-d8ec-0310-ae81-f5f2bcd8cb94

--- tables/tests/test_all.py
+++ tables/tests/test_all.py
@@ -131,8 +131,8 @@ Performing the complete test suite!"""
 Performing only a light (yet comprehensive) subset of the test suite.
 If you want a more complete test, try passing the --heavy flag to this script
 (or set the 'heavy' parameter in case you are using tables.test() call).
-The whole suite will take more than 2 minutes to complete on a relatively
-modern CPU and around 80 MB of main memory."""
+The whole suite will take more than 100 minutes to complete on a relatively
+modern CPU and around 120 MB of main memory."""
     print '-=' * 38
 
 
