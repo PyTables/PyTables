PyTables/PyTables

commit 1683724b6c4e3e59ee11e43ddf6ea3c4d2197140

Added a `forceCSI` argument to Table.itersorted(), Table.readSorted() and Table.copy() in order to be able to force the creation of a CSI index in case that one is not already available.
Also, ptrepack has received a new --forceCSI so that a table can be
sorted in command line, even if it doesn't have a CSI index.  This
implies the modification of the *source* file, but as this flag is
disabled by default, I think this is fine.



git-svn-id: http://www.pytables.org/svn/pytables/PyTablesPro/trunk@3764 1b98710c-d8ec-0310-ae81-f5f2bcd8cb94

tables/table.py
@@ -246,10 +246,10 @@ class Table(tableExtension.Table, Leaf):
     * col(name)
     * iterrows([start][, stop][, step])
     * itersequence(sequence)
-    * itersorted(sortby[, start][, stop][, step])
+    * itersorted(sortby[, forceCSI][, start][, stop][, step])
     * read([start][, stop][, step][, field][, coords])
     * readCoordinates(coords[, field])
-    * readSorted(sortby[, field,][, start][, stop][, step])
+    * readSorted(sortby[, forceCSI][, field,][, start][, stop][, step])
     * __getitem__(key)
     * __iter__()
 
@@ -1318,35 +1318,50 @@ Wrong 'sequence' parameter type. Only sequences are suported.""")
         return row._iter(start, stop, step, coords=sequence)
 
 
-    def _check_sortby_CSI(self, sortby):
-        if sortby not in self.description._v_names:
-            raise (KeyError, "Field `%s` not found in table `%s`" % \
-                   (sortby, self))
-        icol = self.cols._f_col(sortby)
-        if not icol.is_indexed or not icol.is_index_CSI:
-            raise (ValueError,
-                   "Field `%s` must have associated a completely "
-                   "sorted index in table `%s`" % (sortby, self))
-        return icol.index
+    def _check_sortby_CSI(self, sortby, forceCSI):
+        if isinstance(sortby, Column):
+            icol = sortby
+        elif isinstance(sortby, str):
+            icol = self.cols._f_col(sortby)
+        else:
+            raise TypeError, \
+                  "`sortby` can only be a `Column` or string object, " \
+                  "but you passed an object of type: %s" % type(sortby)
+        if icol.is_indexed and icol.is_index_CSI:
+            return icol.index
+        if forceCSI:
+            if icol.is_indexed:
+                # The index exists, but it is not a CSI one.  Remove it.
+                icol.removeIndex()
+            # Create a new CSI index with default parameters.
+            icol.createCSIndex()
+            return icol.index
+        else:
+            if not icol.is_indexed or not icol.is_index_CSI:
+                raise ValueError, \
+                      "Field `%s` must have associated a CSI index " \
+                      "in table `%s`.  You can either create it " \
+                      "separately or pass the `forceCSI` parameter" \
+                      "set to true." % (sortby, self)
 
 
-    def itersorted(self, sortby, start=None, stop=None, step=None):
+    def itersorted(self, sortby, forceCSI=False,
+                   start=None, stop=None, step=None):
         """Iterate over the table data sorted by the given `sortby` column.
 
-        `sortby` is the name of a column that must have associated a
-        completely sorted index (CSI) so as to ensure a fully sorted
-        order.
+        `sortby` column must have associated a completely sorted index
+        (CSI) so as to ensure a fully sorted order.  You can use the
+        `forceCSI` argument in order to force the creation of a CSI
+        index in case that one does not exist yet.
 
-        If you specify a `start`, `stop` and `step` parameters, only
-        this range of values (in the *sorted* order) is returned.
-
-        .. Note:: In this case a negative value of `step` is supported
-        and means that the results will be returned in reverse sorted
-        order.
+        The meaning of the `start`, `stop` and `step` arguments is the
+        same as in `Table.read()`.  However, in this case a negative
+        value of `step` is supported, meaning that the results will be
+        returned in reverse sorted order.
 
         .. Note:: Column indexing is only available in PyTables Pro.
         """
-        index = self._check_sortby_CSI(sortby)
+        index = self._check_sortby_CSI(sortby, forceCSI)
         # Adjust the slice to be used.
         (start, stop, step) = index._processRange(start, stop, step)
         if (start >= stop):
@@ -1355,14 +1370,15 @@ Wrong 'sequence' parameter type. Only sequences are suported.""")
         return row._iter(start, stop, step, coords=index)
 
 
-    def readSorted( self, sortby, field=None,
-                    start=None, stop=None, step=None ):
+    def readSorted(self, sortby, forceCSI=False, field=None,
+                   start=None, stop=None, step=None):
         """
         Read table data sorted by the given `sortby` column.
 
-        `sortby` is the name of a column that must have associated a
-        completely sorted index (CSI) so as to ensure a fully sorted
-        order.
+        `sortby` column must have associated a completely sorted index
+        (CSI) so as to ensure a fully sorted order.  You can use the
+        `forceCSI` argument in order to force the creation of a CSI
+        index in case that one does not exist yet.
 
         If `field` is supplied only the named column will be selected.
         If the column is not nested, an *array* of the current flavor
@@ -1370,17 +1386,15 @@ Wrong 'sequence' parameter type. Only sequences are suported.""")
         instead.  If no `field` is specified, all the columns will be
         returned in a record array of the current flavor.
 
-        If you specify a `start`, `stop` and `step` parameters, only
-        this range of values (in the *sorted* order) is returned.
-
-        .. Note:: In this case a negative value of `step` is supported
-        and means that the results will be returned in reverse sorted
-        order.
+        The meaning of the `start`, `stop` and `step` arguments is the
+        same as in `Table.read()`.  However, in this case a negative
+        value of `step` is supported, meaning that the results will be
+        returned in reverse sorted order.
 
         .. Note:: Column indexing is only available in PyTables Pro.
         """
         self._checkFieldIfNumeric(field)
-        index = self._check_sortby_CSI(sortby)
+        index = self._check_sortby_CSI(sortby, forceCSI)
         coords = index[start:stop:step]
         return self.readCoordinates(coords, field)
 
@@ -2212,7 +2226,7 @@ The 'names' parameter must be a list of strings.""")
         self._doReIndex(dirty=True)
 
 
-    def _g_copyRows(self, object, start, stop, step, sortby):
+    def _g_copyRows(self, object, start, stop, step, sortby, forceCSI):
         "Copy rows from self to object"
         if sortby is None:
             self._g_copyRows_optim(object, start, stop, step)
@@ -2227,7 +2241,9 @@ The 'names' parameter must be a list of strings.""")
             if sortby is None:
                 rows = self[start2:stop2:step]
             else:
-                rows = self.readSorted(sortby, None, start2, stop2, step)
+                rows = self.readSorted(
+                    sortby=sortby, forceCSI=forceCSI, field=None,
+                    start=start2, stop=stop2, step=step )
             # Save the records on disk
             object.append(rows)
         object.flush()
@@ -2274,6 +2290,7 @@ The 'names' parameter must be a list of strings.""")
         # Get the private args for the Table flavor of copy()
         sortby = kwargs.pop('sortby', None)
         propindexes = kwargs.pop('propindexes', False)
+        forceCSI = kwargs.pop('forceCSI', False)
         # Compute the correct indices.
         (start, stop, step) = self._processRangeRead(
             start, stop, step, warn_negstep = sortby is None)
@@ -2281,7 +2298,7 @@ The 'names' parameter must be a list of strings.""")
         newtable = Table( group, name, self.description, title=title,
                           filters=filters, expectedrows=self.nrows,
                           _log=_log )
-        self._g_copyRows(newtable, start, stop, step, sortby)
+        self._g_copyRows(newtable, start, stop, step, sortby, forceCSI)
         nbytes = newtable.nrows * newtable.rowsize
         # Generate equivalent indexes in the new table, if required.
         if propindexes and self.indexed:
@@ -2296,16 +2313,22 @@ The 'names' parameter must be a list of strings.""")
         """ Copy this table and return the new one.
 
         This method has the behavior and keywords described in
-        `Leaf.copy()`.  Moreover, this method recognises the next
-        additional keyword arguments:
+        `Leaf.copy()`.  Moreover, it recognises the next additional
+        keyword arguments:
 
         `sortby`
             If specified, and `sortby` corresponds to a column with a
             completely sorted index (CSI), then the copy will be sorted
             by the values on this column.  A reverse sorted copy can be
             achieved by specifying a negative value for the `step`
-            keyword.  If omitted or ``None``, the original table order
-            is used.
+            keyword.  If `sortby` is omitted or ``None``, the original
+            table order is used.
+        `forceCSI`
+            If true, and a CSI index does not exist for the `sortby`
+            column, one will be built prior to method execution.  If
+            false, the CSI creation will not be forced (this may cause
+            the raise of an error).  In case a CSI index already exists
+            for the `sortby` column, this parameter does nothing.
         `propindexes`
             If true, the existing indexes in the source table are
             propagated (created) to the new one.  If false (the
@@ -2544,7 +2567,7 @@ class Cols(object):
             raise TypeError, \
 "Parameter can only be an string. You passed object: %s" % colname
         if ((colname.find('/') > -1 and
-             not colname in self._v_colpathnames) and
+             not colname in self._v_colpathnames) or
             not colname in self._v_colnames):
             raise KeyError(
 "Cols accessor ``%s.cols%s`` does not have a column named ``%s``"
@@ -3012,24 +3035,24 @@ class Column(object):
         _checkIndexingAvailable()
         kinds = ['ultralight', 'light', 'medium', 'full']
         if kind not in kinds:
-            raise (ValueError,
-                   "Kind must have any of these values: %s" % kinds)
+            raise ValueError, \
+                  "Kind must have any of these values: %s" % kinds
         if (not isinstance(optlevel, (int, long)) or
             (optlevel < 0 or optlevel > 9)):
-            raise (ValueError,
-                   "Optimization level must be an integer in the range 0-9")
+            raise ValueError, \
+                  "Optimization level must be an integer in the range 0-9"
         if filters is None:
             filters = defaultIndexFilters
         if tmp_dir is None:
             tmp_dir = os.path.dirname(self._tableFile.filename)
         else:
             if not os.path.isdir(tmp_dir):
-                raise (ValueError,
-                       "Temporary directory '%s' does not exist" % tmp_dir)
+                raise ValueError, \
+                      "Temporary directory '%s' does not exist" % tmp_dir
         if (_blocksizes is not None and
             (type(_blocksizes) is not tuple or len(_blocksizes) != 4)):
-            raise (ValueError,
-                   "_blocksizes must be a tuple with exactly 4 elements")
+            raise ValueError, \
+                  "_blocksizes must be a tuple with exactly 4 elements"
         idxrows = _column__createIndex(self, kind, optlevel, filters,
                                        tmp_dir, _blocksizes, _verbose)
         return SizeType(idxrows)

debian/ptrepack.1
@@ -104,6 +104,11 @@ an existing index in \fIcolumn\fR.  For reversing the order, use a
 negative value in the \fIstep\fR part of \fIRANGE\fR (see \fI\-R\fR
 flag).  Only applies to table objects.
 .TP
+.BI  \-\-forceCSI
+Force the creation of a CSI index in case one is not available for the
+\-\-sortby column (this implies the modification of the \fIsource\fR
+file).  The default is to not create it.
+.TP
 .BI  \-\-propindexes
 Propagate the indexes existing in original tables.  The default is to
 not propagate them.  Only applies to table objects.

doc/xml/usersguide.xml
@@ -1862,10 +1862,10 @@ class Table(tableExtension.Table, tables.leaf.Leaf)
  |  * col(name)
  |  * iterrows([start][, stop][, step])
  |  * itersequence(sequence)
-    * itersorted(sortby[, start][, stop][, step])
+    * itersorted(sortby[, forceCSI][, start][, stop][, step])
  |  * read([start][, stop][, step][, field][, coords])
  |  * readCoordinates(coords[, field])
-    * readSorted(sortby[, field,][, start][, stop][, step])
+    * readSorted(sortby[, forceCSI][, field,][, start][, stop][, step])
  |  * __getitem__(key)
  |  * __iter__()
  |  
@@ -6776,26 +6776,24 @@ for node in h5file.root.detector:
           </section>
 
           <section id="Table.itersorted">
-            <title>itersorted(sortby, start=None, stop=None,
+            <title>itersorted(sortby, forceCSI=False, start=None, stop=None,
             step=None)</title>
 
             <para>Iterate over the table data sorted by the given
             <literal>sortby</literal> column.</para>
 
-            <para><literal>sortby</literal> is the name of a column that must
-            have associated a completely sorted index (CSI) so as to ensure a
-            fully sorted order.</para>
+            <para><literal>sortby</literal> column must have associated a
+            completely sorted index (CSI) so as to ensure a fully sorted
+            order.  You can use the <literal>forceCSI</literal> argument in
+            order to force the creation of a CSI index in case that one does
+            not exist yet.</para>
 
-            <para>If you specify a <literal>start</literal>,
-            <literal>stop</literal> and <literal>step</literal> parameters,
-            only this range of values (in the sorted order) is
-            returned.</para>
-
-            <note>
-              <para>In this case a negative value of <literal>step</literal>
-              is supported and means that the results will be returned in
-              reverse <emphasis>sorted</emphasis> order.</para>
-            </note>
+            <para>The meaning of the <literal>start</literal>,
+            <literal>stop</literal> and <literal>step</literal> arguments is
+            the same as in <literal>Table.read()</literal> (see <xref
+            linkend="Table.read" />).  However, in this case a negative value
+            of <literal>step</literal> is supported, meaning that the results
+            will be returned in reverse sorted order.</para>
 
             <note role="pro">
               <para>Column indexing is only available in PyTables Pro.</para>
@@ -6856,15 +6854,17 @@ for node in h5file.root.detector:
           </section>
 
           <section id="Table.readSorted">
-            <title>readSorted(sortby, field=None, start=None, stop=None,
-            step=None)</title>
+            <title>readSorted(sortby, forceCSI=False, field=None, start=None,
+            stop=None, step=None)</title>
 
             <para>Read table data sorted by the given
             <literal>sortby</literal> column.</para>
 
-            <para><literal>sortby</literal> is the name of a column that must
-            have associated a completely sorted index (CSI) so as to ensure a
-            fully sorted order.</para>
+            <para><literal>sortby</literal> column must have associated a
+            completely sorted index (CSI) so as to ensure a fully sorted
+            order.  You can use the <literal>forceCSI</literal> argument in
+            order to force the creation of a CSI index in case that one does
+            not exist yet.</para>
 
             <para>If <literal>field</literal> is supplied only the named
             column will be selected.  If the column is not nested, an
@@ -6874,16 +6874,12 @@ for node in h5file.root.detector:
             columns will be returned in a record array of the current
             flavor.</para>
 
-            <para>If you specify a <literal>start</literal>,
-            <literal>stop</literal> and <literal>step</literal> parameters,
-            only this range of values (in the <emphasis>sorted</emphasis>
-            order) is returned.</para>
-
-            <note>
-              <para>In this case a negative value of <literal>step</literal>
-              is supported and means that the results will be returned in
-              reverse <emphasis>sorted</emphasis> order.</para>
-            </note>
+            <para>The meaning of the <literal>start</literal>,
+            <literal>stop</literal> and <literal>step</literal> arguments is
+            the same as in <literal>Table.read()</literal> (see <xref
+            linkend="Table.read" />).  However, in this case a negative value
+            of <literal>step</literal> is supported, meaning that the results
+            will be returned in reverse sorted order.</para>
 
             <note role="pro">
               <para>Column indexing is only available in PyTables Pro.</para>
@@ -7347,7 +7343,7 @@ table.modifyRows(start=1, stop=3, step=2, rows=rows)</screen>
 
             <para>This method has the behavior and keywords described in
             <literal>Leaf.copy()</literal> (see <xref linkend="Leaf.copy" />.
-            Moreover, this method recognises the next additional keyword
+            Moreover, it recognises the next additional keyword
             arguments:</para>
 
             <glosslist>
@@ -7363,6 +7359,20 @@ table.modifyRows(start=1, stop=3, step=2, rows=rows)</screen>
                   a negative value for the <literal>step</literal> keyword.
                   If omitted or <literal>None</literal>, the original table
                   order is used.</para>
+                </glossdef>
+              </glossentry>
+
+              <glossentry>
+                <glossterm><emphasis
+                role="bold">forceCSI</emphasis></glossterm>
+
+                <glossdef>
+                  <para>If true, and a CSI index does not exist for the
+                  <literal>sortby</literal> column, one will be built prior to
+                  method execution.  If false, the CSI creation will not be
+                  forced (this may cause the raise of an error).  In case a
+                  CSI index already exists for the <literal>sortby</literal>
+                  column, this parameter does nothing.</para>
                 </glossdef>
               </glossentry>
 
@@ -10388,6 +10398,11 @@ TypeError: __init__() got an unexpected keyword argument 'foobar'</screen>
                   <para>The <literal>Column</literal> (see <xref
                   linkend="ColumnClassDescr" xrefstyle="select: label" />)
                   instance for the indexed column.</para>
+
+                  <note role="pro">
+                    <para>Column indexing is only available in PyTables
+                    Pro.</para>
+                  </note>
                 </glossdef>
               </glossentry>
 

tables/scripts/ptrepack.py
@@ -71,9 +71,12 @@ def recreateIndexes(table, dstfileh, dsttable):
 
 def copyLeaf(srcfile, dstfile, srcnode, dstnode, title,
              filters, copyuserattrs, overwritefile, overwrtnodes, stats,
-             start, stop, step, sortby, propindexes, upgradeflavors):
+             start, stop, step, sortby, forceCSI, propindexes, upgradeflavors):
     # Open the source file
-    srcfileh = openFile(srcfile, "r")
+    if forceCSI:
+        srcfileh = openFile(srcfile, 'a')
+    else:
+        srcfileh = openFile(srcfile, 'r')
     # Get the source node (that should exist)
     srcNode = srcfileh.getNode(srcnode)
 
@@ -91,7 +94,7 @@ def copyLeaf(srcfile, dstfile, srcnode, dstnode, title,
         dstleaf = srcNode.name
     # Check whether the destination group exists or not
     if os.path.isfile(dstfile) and not overwritefile:
-        dstfileh = openFile(dstfile, "a")
+        dstfileh = openFile(dstfile, 'a')
         try:
             dstGroup = dstfileh.getNode(dstgroup)
         except:
@@ -113,7 +116,7 @@ def copyLeaf(srcfile, dstfile, srcnode, dstnode, title,
                     raise RuntimeError, "Please check that the node names are not duplicated in destination, and if so, add the --overwrite-nodes flag if desired."
     else:
         # The destination file does not exist or will be overwritten.
-        dstfileh = openFile(dstfile, "w", title=title, filters=filters)
+        dstfileh = openFile(dstfile, 'w', title=title, filters=filters)
         dstGroup = newdstGroup(dstfileh, dstgroup, title="", filters=filters)
 
     # Finally, copy srcNode to dstNode
@@ -122,7 +125,7 @@ def copyLeaf(srcfile, dstfile, srcnode, dstnode, title,
             dstGroup, dstleaf, filters = filters,
             copyuserattrs = copyuserattrs, overwrite = overwrtnodes,
             stats = stats, start = start, stop = stop, step = step,
-            sortby = sortby, propindexes = propindexes)
+            sortby = sortby, forceCSI = forceCSI, propindexes = propindexes)
     except:
         (type, value, traceback) = sys.exc_info()
         print "Problems doing the copy from '%s:%s' to '%s:%s'" % \
@@ -151,17 +154,20 @@ def copyLeaf(srcfile, dstfile, srcnode, dstnode, title,
 def copyChildren(srcfile, dstfile, srcgroup, dstgroup, title,
                  recursive, filters, copyuserattrs, overwritefile,
                  overwrtnodes, stats, start, stop, step,
-                 sortby, propindexes, upgradeflavors):
+                 sortby, forceCSI, propindexes, upgradeflavors):
     "Copy the children from source group to destination group"
     # Open the source file with srcgroup as rootUEP
-    srcfileh = openFile(srcfile, "r", rootUEP=srcgroup)
+    if forceCSI:
+        srcfileh = openFile(srcfile, 'a', rootUEP=srcgroup)
+    else:
+        srcfileh = openFile(srcfile, 'r', rootUEP=srcgroup)
     #  Assign the root to srcGroup
     srcGroup = srcfileh.root
 
     created_dstGroup = False
     # Check whether the destination group exists or not
     if os.path.isfile(dstfile) and not overwritefile:
-        dstfileh = openFile(dstfile, "a")
+        dstfileh = openFile(dstfile, 'a')
         try:
             dstGroup = dstfileh.getNode(dstgroup)
         except:
@@ -184,7 +190,7 @@ def copyChildren(srcfile, dstfile, srcgroup, dstgroup, title,
                     raise RuntimeError, "Please check that the node names are not duplicated in destination, and if so, add the --overwrite-nodes flag if desired."
     else:
         # The destination file does not exist or will be overwritten.
-        dstfileh = openFile(dstfile, "w", title=title, filters=filters)
+        dstfileh = openFile(dstfile, 'w', title=title, filters=filters)
         dstGroup = newdstGroup(dstfileh, dstgroup, title="", filters=filters)
         created_dstGroup = True
 
@@ -198,7 +204,7 @@ def copyChildren(srcfile, dstfile, srcgroup, dstgroup, title,
             dstGroup, recursive = recursive, filters = filters,
             copyuserattrs = copyuserattrs, overwrite = overwrtnodes,
             stats = stats, start = start, stop = stop, step = step,
-            sortby = sortby, propindexes = propindexes)
+            sortby = sortby, forceCSI = forceCSI, propindexes = propindexes)
     except:
         (type, value, traceback) = sys.exc_info()
         print "Problems doing the copy from '%s:%s' to '%s:%s'" % \
@@ -230,7 +236,7 @@ def main():
     global verbose
     global regoldindexes
 
-    usage = """usage: %s [-h] [-v] [-o] [-R start,stop,step] [--non-recursive] [--dest-title=title] [--dont-copyuser-attrs] [--overwrite-nodes] [--complevel=(0-9)] [--complib=lib] [--shuffle=(0|1)] [--fletcher32=(0|1)] [--keep-source-filters] [--upgrade-flavors] [--dont-regenerate-old-indexes] [--sortby=column] [--propindexes] sourcefile:sourcegroup destfile:destgroup
+    usage = """usage: %s [-h] [-v] [-o] [-R start,stop,step] [--non-recursive] [--dest-title=title] [--dont-copyuser-attrs] [--overwrite-nodes] [--complevel=(0-9)] [--complib=lib] [--shuffle=(0|1)] [--fletcher32=(0|1)] [--keep-source-filters] [--upgrade-flavors] [--dont-regenerate-old-indexes] [--sortby=column] [--forceCSI] [--propindexes] sourcefile:sourcegroup destfile:destgroup
      -h -- Print usage message.
      -v -- Show more information.
      -o -- Overwite destination file.
@@ -263,6 +269,9 @@ def main():
          This requires an existing index in "column".  For reversing the order,
          use a negative value in the "step" part of "RANGE" (see "-R" flag).
          Only applies to table objects.
+     --forceCSI -- Force the creation of a CSI index in case one is not
+         available for the --sortby column (this implies the modification of
+         the *source* file).  The default is to not create it.
      --propindexes -- Propagate the indexes existing in original tables.  The
          default is to not propagate them.  Only applies to table objects.
     \n""" % os.path.basename(sys.argv[0])
@@ -282,6 +291,7 @@ def main():
                                      'upgrade-flavors',
                                      'dont-regenerate-old-indexes',
                                      'sortby=',
+                                     'forceCSI',
                                      'propindexes',
                                      ])
     except:
@@ -304,6 +314,7 @@ def main():
     overwrtnodes = False
     upgradeflavors = False
     sortby = None
+    forceCSI = False
     propindexes = False
 
     # Get the options
@@ -350,6 +361,8 @@ def main():
             sortby = option[1]
         elif option[0] == '--propindexes':
             propindexes = True
+        elif option[0] == '--forceCSI':
+            forceCSI = True
         else:
             print option[0], ": Unrecognized option"
             sys.stderr.write(usage)
@@ -421,6 +434,7 @@ def main():
         print "Applying filters:", filters
         if sortby is not None:
             print "Sorting table(s) by column:", sortby
+            print "Forcing a CSI creation:", forceCSI
         if propindexes:
             print "Recreating indexes in copied table(s)"
         print "Start copying %s:%s to %s:%s" % (srcfile, srcnode,
@@ -442,7 +456,7 @@ def main():
             copyuserattrs = copyuserattrs, overwritefile = overwritefile,
             overwrtnodes = overwrtnodes, stats = stats,
             start = start, stop = stop, step = step,
-            sortby = sortby, propindexes = propindexes,
+            sortby = sortby, forceCSI = forceCSI, propindexes = propindexes,
             upgradeflavors=upgradeflavors)
     else:
         # If not a Group, it should be a Leaf
@@ -451,7 +465,7 @@ def main():
             title = title, filters = filters, copyuserattrs = copyuserattrs,
             overwritefile = overwritefile, overwrtnodes = overwrtnodes,
             stats = stats, start = start, stop = stop, step = step,
-            sortby = sortby, propindexes = propindexes,
+            sortby = sortby, forceCSI = forceCSI, propindexes = propindexes,
             upgradeflavors=upgradeflavors)
 
     # Gather some statistics

commit 12fd2606c9d9a9bdefb98363a26d82c253484957

Fixed a glitch introduced in r3764.
git-svn-id: http://www.pytables.org/svn/pytables/PyTablesPro/trunk@3768 1b98710c-d8ec-0310-ae81-f5f2bcd8cb94

tables/table.py
@@ -2567,7 +2567,7 @@ class Cols(object):
             raise TypeError, \
 "Parameter can only be an string. You passed object: %s" % colname
         if ((colname.find('/') > -1 and
-             not colname in self._v_colpathnames) or
+             not colname in self._v_colpathnames) and
             not colname in self._v_colnames):
             raise KeyError(
 "Cols accessor ``%s.cols%s`` does not have a column named ``%s``"