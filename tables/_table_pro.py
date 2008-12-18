PyTables/PyTables

commit 093f95f7e69ce67309c13c371ff35aefd148a796

Small optimizations for the case where there are no results in indexed queries.
git-svn-id: http://www.pytables.org/svn/pytables/PyTablesPro/trunk@3932 1b98710c-d8ec-0310-ae81-f5f2bcd8cb94

tables/_table_pro.py
@@ -177,6 +177,7 @@ def _table__whereIndexed(self, compiled, condvars, start, stop, step):
 
     # Compute the chunkmap for every index in indexed expression
     cmvars = {}
+    tcoords = 0
     for i, idxexpr in enumerate(idxexprs):
         var, ops, lims = idxexpr
         col = condvars[var]
@@ -187,6 +188,7 @@ def _table__whereIndexed(self, compiled, condvars, start, stop, step):
         # Get the number of rows that the indexed condition yields.
         range_ = index.getLookupRange(ops, lims)
         ncoords = index.search(range_)
+        tcoords += ncoords
         if index.reduction == 1 and ncoords == 0:
             # No values from index condition, thus the chunkmap should be empty
             nrowsinchunk = self.chunkshape[0]
@@ -198,9 +200,14 @@ def _table__whereIndexed(self, compiled, condvars, start, stop, step):
         # Assign the chunkmap to the cmvars dictionary
         cmvars["e%d"%i] = chunkmap
 
+    # No candidates found in any indexed expression component, so leave now
+    if tcoords == 0:
+        return iter([])
+
     # Compute the final chunkmap
     chunkmap = numexpr.evaluate(strexpr, cmvars)
-    if chunkmap.sum() == 0:
+    # Method .any() is twice as faster than method .sum()
+    if not chunkmap.any():
         # The chunkmap is empty
         return iter([])
 

tables/index.py
@@ -1763,10 +1763,11 @@ class Index(NotLoggedMixin, indexesExtension.Index, Group):
         if self.dirtycache:
             self.restorecache()
 
-        # An empty item means that the number of records is always
-        # going to be empty, so we avoid further computation
-        # (including looking up the limits cache).
-        if not item:
+        # An empty item or if left limit is larger than the right one
+        # means that the number of records is always going to be empty,
+        # so we avoid further computation (including looking up the
+        # limits cache).
+        if not item or item[0] > item[1]:
             self.starts[:] = 0
             self.lengths[:] = 0
             return 0

commit 52861d894a72bae9fc647b6d26fad6e314f2819d

Fixed a bug in an optimization introduced in r3932.
git-svn-id: http://www.pytables.org/svn/pytables/PyTablesPro/trunk@3960 1b98710c-d8ec-0310-ae81-f5f2bcd8cb94

tables/_table_pro.py
@@ -200,8 +200,8 @@ def _table__whereIndexed(self, compiled, condvars, start, stop, step):
         # Assign the chunkmap to the cmvars dictionary
         cmvars["e%d"%i] = chunkmap
 
-    # No candidates found in any indexed expression component, so leave now
-    if tcoords == 0:
+    if index.reduction == 1 and tcoords == 0:
+        # No candidates found in any indexed expression component, so leave now
         return iter([])
 
     # Compute the final chunkmap