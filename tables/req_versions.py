f65514bf498fc8e68c0d4985568d63392e5ab6b9
Francesc Alted <faltet@pytables.org>
2010-11-05 10:59:18 +0000

    Added missing req_versions.py
    git-svn-id: http://www.pytables.org/svn/pytables/PyTablesPro/branches/pro-2.2@4626 1b98710c-d8ec-0310-ae81-f5f2bcd8cb94

---
+++ tables/req_versions.py
@@ -0,0 +1,25 @@
+########################################################################
+#
+#       License:        BSD
+#       Created:        November 5, 2010
+#       Author:  Francesc Alted - faltet@pytables.com
+#
+########################################################################
+
+"""Required versions for PyTables dependencies.
+
+"""
+
+#***************************************************************
+#  Keep these in sync with setup.py and user's guide and README
+#***************************************************************
+
+# Minimum recommended versions for mandatory packages
+min_numpy_version = '1.4.1'
+min_numexpr_version = '1.4.1'
+min_cython_version = '0.13'
+min_hdf5_version = (1,6,5)  # necessary for allowing 1.6.10 > 1.6.5
+
+# Minimum recommended versions for optional packages
+min_numeric_version = "24.2"
+min_numarray_version = "1.5.2"
