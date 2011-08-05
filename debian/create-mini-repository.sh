9a10c47a1ee702e718b829436f2f04557e8e634f
Antonio Valentino <antonio.valentino@tiscali.it>
2011-08-05 17:28:46 +0200

    Removed debian folder

--- debian/create-mini-repository.sh
+++
@@ -1,6 +0,0 @@
-rm -rf pytables-pro-*-2*
-mkdir binary source
-mv *.deb binary
-mv pytables* source
-dpkg-scanpackages binary /dev/null | gzip -9c > Packages.gz
-dpkg-scansources source /dev/null | gzip -9c > Sources.gz
