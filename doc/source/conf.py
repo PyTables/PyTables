1cea0039b66bb4b0e4d0764526a77e0bbd295e54
Antonio Valentino <antonio.valentino@tiscali.it>
2011-08-10 12:29:27 +0200

    Use a logo in vector format for the PDF usersguide

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -201,7 +201,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-latex_logo = '_static/logo-pytables-small.png'
+latex_logo = 'usersguide/images/pytables-front-logo.pdf'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
