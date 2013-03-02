aca834449910cd358ea59a73984f895cbfc67030
Antonio Valentino <antonio.valentinoi@tiscali.it>
2013-03-02 19:27:46 +0100

    Fix keyword names in example code

--- examples/index.py
+++ examples/index.py
@@ -19,7 +19,7 @@ for i in range(nrows):
     r.append()
 table.flush()
 
-table.cols.frame.createIndex(optlevel=9, testmode=True, verbose=True)
+table.cols.frame.createIndex(optlevel=9, _testmode=True, _verbose=True)
 #table.cols.frame.optimizeIndex(level=5, verbose=1)
 
 results = [r.nrow for r in table.where('frame < 2')]
