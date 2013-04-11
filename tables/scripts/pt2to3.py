ff8f473fc516ac0180fee4031adf0fafc1138599
Anthony Scopatz <scopatz@gmail.com>
2013-04-10 19:15:04 -0500

    some pt2to3 cleanup

--- tables/scripts/pt2to3.py
+++ tables/scripts/pt2to3.py
@@ -37,11 +37,19 @@ def make_subs(ns):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='PyTables 2.x -> 3.x API transition tool')
+    desc = ('PyTables 2.x -> 3.x API transition tool\n\n'
+            'This tool displays to standard out, so it is \n'
+            'common to pipe this to another file:\n\n'
+            '$ pt2to3 oldfile.py > newfile.py')
+    parser = argparse.ArgumentParser(description=desc)
     parser.add_argument('-r', '--reverse', action='store_true', default=False, 
                         dest='reverse', help="reverts changes, going from 3.x -> 2.x.")
     parser.add_argument('-p', '--no-ignore-previous', action='store_false', 
-                default=True, dest='ignore_previous', help="i previous_api() calls.")
+                default=True, dest='ignore_previous', help="ingores previous_api() calls.")
+    parser.add_argument('-o', default=None, dest='output', 
+                        help="output file to write to.")
+    parser.add_argument('-i', '--inplace', action='store_true', default=False, 
+                        dest='inplace', help="overwrites the file in-place.")
     parser.add_argument('filename', help='path to input file.')
     ns = parser.parse_args()
 
@@ -52,7 +60,13 @@ def main():
 
     subs, repl = make_subs(ns)
     targ = subs.sub(repl, src)
-    sys.stdout.write(targ)
+
+    ns.output = ns.filename if ns.inplace else ns.output
+    if ns.output is None:
+        sys.stdout.write(targ)
+    else:
+        with open(ns.output, 'w') as f:
+            f.write(ns.output)
 
 if __name__ == '__main__':
     main()
