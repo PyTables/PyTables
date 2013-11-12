a422f66660e27f7c2f3942fb2afc87a1f2723ff4
Andrea Bedini <andrea.bedini@gmail.com>
2013-11-13 10:25:37 +1100

    Add a test case for keysort function

---
+++ tables/tests/test_aux.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+import numpy
+
+from tables import indexesextension
+
+
+class TestAuxiliaryFunctions(unittest.TestCase):
+    def test_keysort(self):
+        N = 1000
+        rnd = numpy.random.randint(N, size=N)
+        for dtype1 in ('S6', 'b1', 'i1', 'i8', 'u4', 'u8', 'f4', 'f8'):
+            for dtype2 in ('u4', 'i8'):
+                a = numpy.array(rnd, dtype1)
+                b = numpy.array(rnd, dtype2)
+
+                c = a.copy()
+                d = c.argsort()
+                e = c[d]
+                f = b[d]
+
+                indexesextension.keysort(a, b)
+                self.assertTrue((a == e).all())
+                self.assertTrue((b == f).all())
+
+
+def suite():
+    theSuite = unittest.TestSuite()
+    theSuite.addTest(unittest.makeSuite(TestAuxiliaryFunctions))
+    return theSuite
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
