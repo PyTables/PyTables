3ba9db0a1d3dd17e64409d4252d4606740937f4c
Antonio Valentino <antonio.valentino@tiscali.it>
2014-01-15 13:25:39 +0100

    Blosc updated to v1.3.1

--- blosc/blosc.h
+++ blosc/blosc.h
@@ -14,9 +14,9 @@
 /* Version numbers */
 #define BLOSC_VERSION_MAJOR    1    /* for major interface/format changes  */
 #define BLOSC_VERSION_MINOR    3    /* for minor interface/format changes  */
-#define BLOSC_VERSION_RELEASE  0    /* for tweaks, bug-fixes, or development */
+#define BLOSC_VERSION_RELEASE  1    /* for tweaks, bug-fixes, or development */
 
-#define BLOSC_VERSION_STRING   "1.3.0"  /* string version.  Sync with above! */
+#define BLOSC_VERSION_STRING   "1.3.1"  /* string version.  Sync with above! */
 #define BLOSC_VERSION_REVISION "$Rev$"   /* revision version */
 #define BLOSC_VERSION_DATE     "$Date:: 2013-05-17 #$"    /* date version */
 
