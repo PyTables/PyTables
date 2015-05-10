53257b9a9dd23511965f5bc7ff7830a2ca6de256
Andrea Bedini <andreabedini@users.noreply.github.com>
2015-05-10 21:49:34 +1000

    Delete linkExtension.py

--- tables/linkExtension.py
+++
@@ -1,6 +0,0 @@
-from warnings import warn
-from tables.linkextension import *
-
-_warnmsg = ("linkExtension is pending deprecation, import linextension instead. "
-            "You may use the pt2to3 tool to update your source code.")
-warn(_warnmsg, DeprecationWarning, stacklevel=2)
