a772af0db73b9dfc19ea27309e8a0b3e4fb9ee3f
Andrea Bedini <andreabedini@users.noreply.github.com>
2015-05-10 21:49:57 +1000

    Delete utilsExtension.py

--- tables/utilsExtension.py
+++
@@ -1,6 +0,0 @@
-from warnings import warn
-from tables.utilsextension import *
-
-_warnmsg = ("utilsExtension is pending deprecation, import utilsextension instead. "
-            "You may use the pt2to3 tool to update your source code.")
-warn(_warnmsg, DeprecationWarning, stacklevel=2)
