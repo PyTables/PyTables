00b1615a41de490a14d41c23d9386e3cc12121d9
Andrea Bedini <andreabedini@users.noreply.github.com>
2015-05-10 21:48:53 +1000

    Delete hdf5Extension.py

--- tables/hdf5Extension.py
+++
@@ -1,6 +0,0 @@
-from warnings import warn
-from tables.hdf5extension import *
-
-_warnmsg = ("hdf5Extension is pending deprecation, import hdf5extension instead. "
-            "You may use the pt2to3 tool to update your source code.")
-warn(_warnmsg, DeprecationWarning, stacklevel=2)
