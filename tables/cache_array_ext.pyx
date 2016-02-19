4cdc55b6b6fff8f7c737549e8608db12e7be34d1
Andrea Bedini <andrea.bedini@curtin.edu.au>
2016-02-19 16:48:44 +0800

    Convert CacheArray.initread

--- tables/cache_array_ext.pyx
+++ tables/cache_array_ext.pyx
@@ -1,8 +1,9 @@
-from .definitions cimport herr_t, H5Sclose
+from h5py import h5s
+
+from .definitions cimport hid_t, herr_t
 from .exceptions import HDF5ExtError
 
 cdef extern from "H5ARRAY-opt.h" nogil:
-  herr_t H5ARRAYOinit_readSlice(hid_t dataset_id, hid_t *mem_space_id, hsize_t count)
   herr_t H5ARRAYOread_readBoundsSlice(hid_t dataset_id, hid_t mem_space_id, hid_t type_id, hsize_t irow, hsize_t start, hsize_t stop, void *data)
 
 
@@ -10,28 +11,16 @@ cdef class CacheArray(Array):
   """Container for keeping index caches of 1st and 2nd level."""
 
   cdef initread(self, int nbounds):
-    # "Actions to accelerate the reads afterwards."
-
-    # Precompute the mem_space_id
-    if (H5ARRAYOinit_readSlice(self.dataset_id, &self.mem_space_id,
-                               nbounds) < 0):
-      raise HDF5ExtError("Problems initializing the bounds array data.")
-    return
+     self.mem_space = h5s.create_simple((1, nbounds))
 
   cdef read_slice(self, hsize_t nrow, hsize_t start, hsize_t stop, void *rbuf):
     # "Read an slice of bounds."
 
     if (H5ARRAYOread_readBoundsSlice(
-      self.dataset_id, self.mem_space_id, self.type_id,
+      self.dataset_id, self.mem_space.id, self.type_id,
       nrow, start, stop, rbuf) < 0):
       raise HDF5ExtError("Problems reading the bounds array data.")
     return
-
-  def _g_close(self):
-    super(Array, self)._g_close()
-    # Release specific resources of this class
-    if self.mem_space_id > 0:
-      H5Sclose(self.mem_space_id)
 
 # cdef class CacheArray(Array):
 #   """Container for keeping index caches of 1st and 2nd level."""
