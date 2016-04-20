c6a2dd17fb9a689a344bb12eea055ced3844227f
Francesc Alted <faltet@gmail.com>
2016-04-20 19:06:48 +0200

    Updated README and installation procedures, specially with required versions

--- tables/req_versions.py
+++ tables/req_versions.py
@@ -13,13 +13,13 @@
 from distutils.version import LooseVersion
 
 #**********************************************************************
-#  Keep these in sync with requirements.txt and user's guide and README
+#  Keep these in sync with requirements.txt and user's guide
 #**********************************************************************
 
 # Minimum recommended versions for mandatory packages
-min_numpy_version = LooseVersion('1.7.1')
+min_numpy_version = LooseVersion('1.8.0')
 min_numexpr_version = LooseVersion('2.5.2')
 min_hdf5_version = LooseVersion('1.8.4')
-min_blosc_version = LooseVersion("1.3.0")
+min_blosc_version = LooseVersion("1.4.1")
 min_blosc_bitshuffle_version = LooseVersion("1.8.0")
 """The minumum Blosc version where BitShuffle can be used safely."""
