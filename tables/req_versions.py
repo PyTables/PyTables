0548709da6cef3d6bbbc0abe5fde2571226ed03d
Francesc Alted <faltet@gmail.com>
2016-04-20 18:07:17 +0200

    New version dependencies activated

--- tables/req_versions.py
+++ tables/req_versions.py
@@ -10,11 +10,16 @@
 
 """Required versions for PyTables dependencies."""
 
+from distutils.version import LooseVersion
+
 #**********************************************************************
 #  Keep these in sync with requirements.txt and user's guide and README
 #**********************************************************************
 
 # Minimum recommended versions for mandatory packages
-min_numpy_version = '1.7.1'
-min_numexpr_version = '2.4'
-min_hdf5_version = (1, 8, 4)
+min_numpy_version = LooseVersion('1.7.1')
+min_numexpr_version = LooseVersion('2.5.2')
+min_hdf5_version = LooseVersion('1.8.4')
+min_blosc_version = LooseVersion("1.3.0")
+min_blosc_bitshuffle = LooseVersion("1.8.0")
+"""The minumum Blosc version where BitShuffle can be used safely."""
