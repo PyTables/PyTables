cc26b7c4d8c1dedf0e785fceb007ee84611d4f51
Tom Kooij <tomkooij@tomkooij.nl>
2016-06-24 15:05:21 +0200

    Change VLStringAtom to bytestring
    Only allow bytestring in VLStringAtom(). Update docstring.

--- tables/atom.py
+++ tables/atom.py
@@ -1101,15 +1101,25 @@ class VLStringAtom(_BufferedAtom):
     it to one specific row*, i.e. the :meth:`VLArray.append` method only
     accepts one object when the base atom is of this type.
 
-    Like StringAtom, this class does not make assumptions on the encoding of
-    the string, and raw bytes are stored as is.  Unicode strings are supported
-    as long as no character is out of the ASCII set; otherwise, you will need
-    to *explicitly* convert them to strings before you can save them.  For full
-    Unicode support, using VLUnicodeAtom (see :ref:`VLUnicodeAtom`) is
+    This `class stores bytestrings. It does not make assumptions on the
+    encoding of the string, and raw bytes are stored as is. To store a string
+    you will need to *explicitly* convert it to a bytestring before you can
+    save them::
+
+        >>> s = 'A unicode string: hbar = \u210f'
+        >>> bytestring = s.encode('utf-8')
+        >>> VLArray.append(bytestring)
+
+    and decode when reading::
+
+        >>> bytestring = VLArray[0]
+        >>> s = bytestring.decode('utf-8')
+
+    For full Unicode support, using VLUnicodeAtom (see :ref:`VLUnicodeAtom`) is
     recommended.
 
     Variable-length string atoms do not accept parameters and they cause the
-    reads of rows to always return Python strings.  You can regard vlstring
+    reads of rows to always return Python bytestrings.  You can regard vlstring
     atoms as an easy way to save generic variable length strings.
 
     """
@@ -1119,8 +1129,8 @@ class VLStringAtom(_BufferedAtom):
     base = UInt8Atom()
 
     def _tobuffer(self, object_):
-        if not isinstance(object_, six.string_types):
-            raise TypeError("object is not a string: %r" % (object_,))
+        if not isinstance(object_, six.binary_type):
+            raise TypeError("object is not a bytestring: %r" % (object_,))
         return numpy.string_(object_)
 
     def fromarray(self, array):
