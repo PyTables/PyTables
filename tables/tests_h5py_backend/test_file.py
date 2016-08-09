99875c8a8d15e3930d6f909589cb16c2e45f0191
Thomas A Caswell <tcaswell@bnl.gov>
2016-08-09 20:03:01 +0800

    TST: remove floats due to bit-based roundtripping

--- tables/tests_h5py_backend/test_file.py
+++ tables/tests_h5py_backend/test_file.py
@@ -49,8 +49,6 @@ class Record(tb.IsDescription):
     var1 = tb.StringCol(itemsize=4)  # 4-character String
     var2 = tb.IntCol()      # integer
     var3 = tb.Int16Col()    # short integer
-    var4 = tb.FloatCol()    # double (double-precision)
-    var5 = tb.Float32Col()  # float  (single-precision)
 
 
 class CreateFile(TempFileMixin, unittest.TestCase):
@@ -63,7 +61,7 @@ class CreateFile(TempFileMixin, unittest.TestCase):
     def test_create_table(self):
         table = self.h5file.create_table(self.root, 'atable',
                                          Record, "Table title")
-        row = ('abcd', 0, 1, 1.23, 1.34)
+        row = ('abcd', 0, 1)
         table.append([row] * 10)
 
         assert len(table) == 10
@@ -80,25 +78,21 @@ class ReadTable(TempFileMixin, unittest.TestCase):
         super().setUp()
         self.table = self.h5file.create_table(self.root, 'atable',
                                               Record, "Table title")
-        self.row = ('abcd', 0, 1, 1.23, 1.34)
+        self.row = (b'abcd', 0, 1)
         self.table.append([self.row] * 10)
 
     def test_read_all(self):
         readout = self.table[:]
         assert len(readout) == 10
-        recarr = np.array([self.row]*10, dtype="S4,i4,i2,f8,f4")
+        recarr = np.array([self.row]*10, dtype="S4,i4,i2")
         np.testing.assert_equal(readout['var1'], recarr['f0'])
         for f1, f2 in zip(('f0', 'f1', 'f2'), ('var1', 'var2', 'var3')):
             np.testing.assert_equal(readout[f2], recarr[f1])
-        for f1, f2 in zip(('f3', 'f4'), ('var4', 'var5')):
-            np.testing.assert_almost_equal(readout[f2], recarr[f1])
 
     def test_iter(self):
         for row in self.table:
-            assert row == self.row
-
+            assert row[:] == self.row
 
     def test_iterrows(self):
         for row in self.table.iterrows():
             assert row[:] == self.row
-
