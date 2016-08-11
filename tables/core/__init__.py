9287f5b83f8b71fbde5b319ba4d095bac5f0818a
Thomas A Caswell <tcaswell@bnl.gov>
2016-08-11 16:21:45 +0800

    MNT: be more cautious about mode

--- tables/core/__init__.py
+++ tables/core/__init__.py
@@ -4,7 +4,10 @@ from .group import File
 
 def open_file(*args, title='', **kwargs):
     backend = open_backend(*args, **kwargs)
-    mode = args[1]
+    if len(args) < 2:
+        mode = 'r+'
+    else:
+        mode = args[1]
     if mode == 'w':
         backend.attrs['TITLE'] = title
     return File(backend=backend)
