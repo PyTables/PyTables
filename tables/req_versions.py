763922fa561bd6b6b59ebbfc954b8a5ec6d93508
Thomas A Caswell <tcaswell@gmail.com>
2017-07-05 17:47:31 -0400

    BLD: add h5py as required dependency

--- tables/req_versions.py
+++ tables/req_versions.py
@@ -20,6 +20,7 @@ from distutils.version import LooseVersion
 min_numpy_version = LooseVersion('1.8.0')
 min_numexpr_version = LooseVersion('2.5.2')
 min_hdf5_version = LooseVersion('1.8.4')
+min_h5py_version = LooseVersion('2.7')
 min_blosc_version = LooseVersion("1.4.1")
 min_blosc_bitshuffle_version = LooseVersion("1.8.0")
 """The minumum Blosc version where BitShuffle can be used safely."""
