39c0ddff71f0fef485946d364ec3c14adc93a497
Francesc Alted <faltet@gmail.com>
2018-01-25 10:56:05 +0100

    [FIX] The Blosc version has been exactly set to 1.13.3 (as it should)

--- c-blosc/blosc/blosc.h
+++ c-blosc/blosc/blosc.h
@@ -21,9 +21,9 @@ extern "C" {
 #define BLOSC_VERSION_MINOR    13   /* for minor interface/format changes  */
 #define BLOSC_VERSION_RELEASE  3    /* for tweaks, bug-fixes, or development */
 
-#define BLOSC_VERSION_STRING   "1.13.3.dev"  /* string version.  Sync with above! */
+#define BLOSC_VERSION_STRING   "1.13.3"  /* string version.  Sync with above! */
 #define BLOSC_VERSION_REVISION "$Rev$"   /* revision version */
-#define BLOSC_VERSION_DATE     "$Date:: 2018-01-20 #$"    /* date version */
+#define BLOSC_VERSION_DATE     "$Date:: 2018-01-25 #$"    /* date version */
 
 #define BLOSCLZ_VERSION_STRING "1.1.0"   /* the internal compressor version */
 
