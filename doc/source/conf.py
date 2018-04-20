c06fee05867f1e27e5fb8daeae2a1cedf8819129
Javier Sancho <jsf@jsancho.org>
2018-04-20 11:33:14 +0200

    Update docs to v3.4.3

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'PyTables'
-copyright = '2011-2017, PyTables maintainers'
+copyright = '2011-2018, PyTables maintainers'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
--- tables/expression.py
+++ tables/expression.py
@@ -39,6 +39,12 @@ class Expr(object):
     but in addition to NumPy objects, it also accepts disk-based homogeneous
     arrays, like the Array, CArray, EArray and Column PyTables objects.
 
+    .. warning::
+
+        Expr class only offers a subset of the Numexpr features due to the
+        complexity of implement some of them when dealing with huge amount of
+        data.
+
     All the internal computations are performed via the Numexpr package,
     so all the broadcast and upcasting rules of Numexpr applies here too.
     These rules are very similar to the NumPy ones, but with some exceptions
@@ -46,6 +52,7 @@ class Expr(object):
     disk-based arrays.  Be sure to read the documentation of the Expr
     constructor and methods as well as that of Numexpr, if you want to fully
     grasp these particularities.
+
 
     Parameters
     ----------
