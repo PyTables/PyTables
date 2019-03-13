193cc2e3c968d5000e4430baac7aec81e80aa909
Francesc Alted <faltet@gmail.com>
2019-03-13 13:28:13 +0100

    Remove the check completely for non 64-bit builds (enough is enough)

--- tables/tests/test_tables.py
+++ tables/tests/test_tables.py
@@ -4889,9 +4889,6 @@ class CopyTestCase(common.TempFileMixin, TestCase):
             if is_python_64bit():
                 self.assertEqual(table1.description._v_offsets, [0, 8, 16])
                 self.assertEqual(table1.description._v_itemsize, 24)
-            else:
-                self.assertEqual(table1.description._v_offsets, [0, 8, 12])
-                self.assertEqual(table1.description._v_itemsize, 20)
         else:
             self.assertEqual(table1.description._v_offsets, [0, 8, 11])
             self.assertEqual(table1.description._v_itemsize, 19)
