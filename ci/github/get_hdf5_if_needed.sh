9c8114bd447135bcb8e2f682a19fb16e234f4513
Matt Einhorn <matt@einhorn.dev>
2022-03-07 18:10:31 -0500

    Apply script fixes from local testing.

--- ci/github/get_hdf5_if_needed.sh
+++ ci/github/get_hdf5_if_needed.sh
@@ -15,7 +15,8 @@ else
         EXTRA_MPI_FLAGS="--enable-parallel --enable-shared"
     fi
 
-    MINOR_V=$(sed -e "s/.[0-9]\+\$//; s/^[0-9]\+.//" <<< "$HDF5_VERSION")
+    MINOR_V=${HDF5_VERSION#*.}
+    MINOR_V=${MINOR_V%.*}
     MAJOR_V=${HDF5_VERSION/%.*.*}
     if [[ "$OSTYPE" == "darwin"* ]]; then
         lib_name=libhdf5.dylib
@@ -33,9 +34,8 @@ else
             pushd /tmp
 
             # snappy
-            curl -sLO https://github.com/google/snappy/archive/refs/tags/1.1.9.tar.gz
-            tar xzf 1.1.9.tar.gz
-            pushd snappy-1.1.9
+            git clone https://github.com/google/snappy.git --branch 1.1.9 --depth 1
+            pushd snappy
             git submodule update --init
             mkdir build
             cd build
@@ -56,11 +56,15 @@ else
 
             # universal binaries is only supported for v1.13+
             if [[ $MAJOR_V -eq 1 && $MINOR_V -lt 13 ]]; then
-                echo "MACOS universal wheels can only be built with HDF5 version 1.13+" 1>&2
-                exit 1
+              echo "MACOS universal wheels can only be built with HDF5 version 1.13+" 1>&2
+              exit 1
             fi
 
             export CFLAGS="$CFLAGS -arch x86_64 -arch arm64"
+            export CPPFLAGS="$CPPFLAGS -arch x86_64 -arch arm64"
+            export CXXFLAGS="$CXXFLAGS -arch x86_64 -arch arm64"
+            export CC="/usr/bin/clang"
+            export CXX="/usr/bin/clang"
 
             # lz4
             curl -sLO https://github.com/lz4/lz4/archive/refs/tags/v1.9.3.tar.gz
@@ -69,21 +73,34 @@ else
             make install PREFIX="$HDF5_DIR"
             popd
 
-            # lzo
-            curl -sLO https://www.oberhumer.com/opensource/lzo/download/lzo-2.10.tar.gz
-            tar xzf lzo-2.10.tar.gz
-            pushd lzo-2.10
-            ./configure --prefix="$HDF5_DIR"
-            make
-            make install
-            popd
-
             # bzip2
             curl -sLO https://gitlab.com/bzip2/bzip2/-/archive/bzip2-1.0.8/bzip2-bzip2-1.0.8.tar.gz
             tar xzf bzip2-bzip2-1.0.8.tar.gz
             pushd bzip2-bzip2-1.0.8
             make install PREFIX="$HDF5_DIR"
             popd
+
+            # lzo
+            curl -sLO https://www.oberhumer.com/opensource/lzo/download/lzo-2.10.tar.gz
+            tar xzf lzo-2.10.tar.gz
+            pushd lzo-2.10
+            CFLAGS= CXXFLAGS= CPPFLAGS= ./configure --prefix="$HDF5_DIR"_arm64 CFLAGS="-arch arm64" \
+                CXXFLAGS="-arch arm64" --host="aarch64-apple-darwin"  --target="aarch64-apple-darwin"
+            make
+            make install
+            popd
+
+            rm -rf lzo-2.10
+            tar xzf lzo-2.10.tar.gz
+            pushd lzo-2.10
+            CFLAGS= CXXFLAGS= CPPFLAGS= ./configure --prefix="$HDF5_DIR"_x86
+            make
+            make install
+            popd
+
+            lipo "$HDF5_DIR"_x86/lib/liblzo2.a "$HDF5_DIR"_arm64/lib/liblzo2.a -output "$HDF5_DIR"/lib/liblzo2.a -create
+            cp -r "$HDF5_DIR"_x86/include/lzo "$HDF5_DIR"/include/lzo
+            cp "$HDF5_DIR"_x86/lib/pkgconfig/lzo2.pc "$HDF5_DIR"/lib/pkgconfig
 
             # zlib
             curl -sLO https://zlib.net/zlib-1.2.11.tar.gz
@@ -95,6 +112,9 @@ else
             popd
 
             popd
+
+            export CPPFLAGS=
+            export CXXFLAGS=
         fi
 
         pushd /tmp
