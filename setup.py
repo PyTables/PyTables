f1197eb35e1d484f82da4905b212289470014528
Francesc Alted <francesc@blosc.org>
2022-12-09 18:26:21 +0100

    Copy blosc2 shared libs to /tmp/hdf5

--- setup.py
+++ setup.py
@@ -849,6 +849,10 @@ if __name__ == "__main__":
                 if platform_system == "Linux":
                     shutil.copy(libdir / 'libblosc2.so', 'tables')
                     copy_libs += ['libblosc2.so']
+                    if "bdist_wheel" in sys.argv:
+                        shared_libs = glob.glob(str(libdir) + '/libblosc2.so*')
+                        for lib in shared_libs:
+                            shutil.copy(lib, '/tmp/hdf5/lib')
                 elif platform_system == "Darwin":
                     shutil.copy(libdir / 'libblosc2.dylib', 'tables')
                     copy_libs += ['libblosc2.dylib']
