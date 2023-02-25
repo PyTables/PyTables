31c5ddf41aee65fd93adfc0c10686e5cc81a6c0f
Ben Greiner <code@bnavigator.de>
2023-02-24 18:18:56 -0800

    remove blosc2 from runtime requirements

--- tables/req_versions.py
+++ tables/req_versions.py
@@ -3,12 +3,13 @@
 from packaging.version import Version
 
 # **********************************************************************
-#   Keep these in sync with setup.cfg and user's guide
+#   Keep these in sync with requirements.txt and user's guide
 # **********************************************************************
 
 # Minimum recommended versions for mandatory packages
-min_numpy_version = Version('1.9.3')
+min_numpy_version = Version('1.19.0')
 min_numexpr_version = Version('2.6.2')
+# These are library versions, not the python modules
 min_hdf5_version = Version('1.10.5')
 min_blosc_version = Version('1.11.1')
 min_blosc2_version = Version('2.5.0')
