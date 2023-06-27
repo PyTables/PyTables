0821c095a19a77f2e069e9969b9aea5b106cb034
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2023-06-27 13:19:02 +0200

    Revert CArray examples to the original one
    Blosc2 tests will be added as unit tests.

--- examples/carray1.py
+++ examples/carray1.py
@@ -1,27 +1,19 @@
 import numpy as np
 import tables as tb
-import sys
 
 fileName = 'carray1.h5'
 shape = (200, 300)
-atom = tb.UInt16Atom()
-filters = tb.Filters(complevel=5, complib="blosc2:lz4")
+atom = tb.UInt8Atom()
+filters = tb.Filters(complevel=5, complib='zlib')
 
 h5f = tb.open_file(fileName, 'w')
-a = np.arange(np.prod(shape), dtype="uint16").reshape(shape)
-
 ca = h5f.create_carray(h5f.root, 'carray', atom, shape, filters=filters)
-ca[...] = a
-print("chunkshape: ", ca.chunkshape)
-print("shape ", ca.shape)
-print(ca.name)
 # Fill a hyperslab in ``ca``.
-# ca[10:60, 20:70] = np.ones((50, 50))
-print(ca[...])
+ca[10:60, 20:70] = np.ones((50, 50))
 h5f.close()
 
 # Re-open and read another hyperslab
-# h5f = tb.open_file(fileName)
-# print(h5f)
-# print(h5f.root.carray[8:12, 18:22])
-# h5f.close()
+h5f = tb.open_file(fileName)
+print(h5f)
+print(h5f.root.carray[8:12, 18:22])
+h5f.close()
