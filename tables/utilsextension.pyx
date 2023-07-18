bad9aaea89bb23f17edf6f594f7a38857140287a
olifes <134951098+olife-png@users.noreply.github.com>
2023-07-19 03:12:24 +0700

    Revert "python3.12: cython: fix slice indexing"

--- tables/utilsextension.pyx
+++ tables/utilsextension.pyx
@@ -899,12 +899,9 @@ def get_nested_field(recarray, fieldname):
   """Get the maybe nested field named `fieldname` from the `recarray`.
 
   The `fieldname` may be a simple field name or a nested field name
-  with slash-separated components.
+  with slah-separated components.
 
   """
-
-  if not isinstance(fieldname, str):
-    raise TypeError
 
   cdef bytes name = fieldname.encode('utf-8')
   try:
