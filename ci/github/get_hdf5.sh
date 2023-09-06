65d24dec366ee6e55c584bc4b1a9f417009d766f
Clemens Brunner <clemens.brunner@gmail.com>
2023-09-06 14:04:49 +0200

    Disable patches

--- ci/github/get_hdf5.sh
+++ ci/github/get_hdf5.sh
@@ -122,8 +122,8 @@ if [[ "$OSTYPE" == "darwin"* && "$CIBW_ARCHS" = "arm64"  ]]; then
     export hdf5_cv_system_scope_threads=yes
     export hdf5_cv_printf_ll="l"
 
-    patch -p0 < "$PROJECT_DIR/ci/osx_cross_configure.patch"
-    patch -p0 < "$PROJECT_DIR/ci/osx_cross_src_makefile.patch"
+    # patch -p0 < "$PROJECT_DIR/ci/osx_cross_configure.patch"
+    # patch -p0 < "$PROJECT_DIR/ci/osx_cross_src_makefile.patch"
 
     eval ./configure --prefix="$HDF5_DIR" --with-zlib="$HDF5_DIR" "$EXTRA_MPI_FLAGS" "$EXTRA_SERIAL_FLAGS" --enable-build-mode=production \
         --host=aarch64-apple-darwin --enable-tests=no
