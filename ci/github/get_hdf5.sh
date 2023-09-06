ea40eb6eba1d9fa4cf38b861e0b7ff2394cafc5a
Clemens Brunner <clemens.brunner@gmail.com>
2023-09-06 17:33:16 +0200

    Fix tar.gz file name

--- ci/github/get_hdf5.sh
+++ ci/github/get_hdf5.sh
@@ -109,7 +109,7 @@ pushd /tmp
 
 if [[ "$OSTYPE" == "darwin"* && "$CIBW_ARCHS" = "arm64"  ]]; then  # use binary build on macOS ARM64
     curl -fsSLO "https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-${HDF5_VERSION%.*}/hdf5-${HDF5_VERSION%-*}/bin/unix/hdf5-${HDF5_VERSION}-Std-macos11m1_64-clang.tar.gz"
-    tar -xzvf "hdf5-${HDF5_VERSION}-Std-macos11m1_64_clang.tar.gz"
+    tar -xzvf "hdf5-${HDF5_VERSION}-Std-macos11m1_64-clang.tar.gz"
     sh hdf/HDF5-1.14.1-Darwin.sh --skip-license --prefix="$HDF5_DIR" --exclude-subdir
     mv "${HDF5_DIR}/HDF_Group/HDF5/${HDF5_VERSION%-*}/*" "${HDF5_DIR}"
 else
