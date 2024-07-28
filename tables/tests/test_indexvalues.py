35dbaa7657d07a2498bb53988b5c3b22bbe87800
Antonio Valentino <antonio.valentino@tiscali.it>
2024-07-28 19:27:38 +0200

    Fix numpy2 compat for test_indexvalues (Closes: #1185)

--- tables/tests/test_indexvalues.py
+++ tables/tests/test_indexvalues.py
@@ -2296,12 +2296,6 @@ class SelectValuesTestCase(common.TempFileMixin, common.PyTablesTestCase):
         self.assertFalse(t1var3.index.dirty)
         self.assertFalse(t1var4.index.dirty)
 
-        # https://github.com/PyTables/PyTables/issues/1185
-        # TODO: IT IS DIRTY BECAUSE THIS FIXES THINGS FOR FINSV2aTestCase,
-        # which otherwise fails a test a few lines below!
-        for col in table1.colinstances.values():
-            col.reindex()
-
         # Do some selections and check the results
         # First selection: string
         # Convert the limits to the appropriate type
